7184d307cbc6d366e08dab1b75160d6d8d24d132
James Tai <jtai@jtai.ca>
2021-04-28 15:27:33 -0700

    Remove unused import

--- src/main/java/ca/jtai/bettermending/BetterMending.java
+++ src/main/java/ca/jtai/bettermending/BetterMending.java
@@ -1,7 +1,6 @@
 package ca.jtai.bettermending;
 
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
